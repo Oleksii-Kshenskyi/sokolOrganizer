42f42e5549bc7bb774d18660e181ed062e2309d7
gitSokolAV <112345412+gitSokolAV@users.noreply.github.com>
2022-12-24 23:37:18 +0200

    [issue #6] Adding: echo, exit (PR #13)
    [#6] Adding: echo, exit
    [#6] Fixed bug with crashing echo command
    [#6] Fixed access modifiers
    [#6] Fixed access modifiers

---
+++ SokolTextGame/Commands.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SokolTextGame
+{
+    public interface ICommand
+    {
+        public void Execute(string[] words, World world);
+    }
+    public class Echo : ICommand
+    {
+        public void Execute(string[] words, World world)
+        {
+            string text = string.Join(" ", words.Skip(1));
+            Console.WriteLine(text);
+        }
+    }
+
+    public class Exit : ICommand
+    {
+        public void Execute(string[] words, World world)
+        {
+            Console.WriteLine("See you later..");
+            Environment.Exit(0);
+        }
+    }
+
+    public class Unknown : ICommand
+    {
+        public void Execute(string[] words, World world)
+        {
+            Console.WriteLine("I'm sorry, but there is no such command. =) ");
+        }
+    }
+}
---
+++ SokolTextGame/Program.cs
@@ -0,0 +1,10 @@
+﻿using SokolTextGame;
+
+World? world = new World();
+
+while (true)
+{
+    Console.Write("~~>> ");
+    string? userInput = Console.ReadLine();
+    if (!string.IsNullOrEmpty(userInput)) world.ExecuteCommand(userInput);
+}
