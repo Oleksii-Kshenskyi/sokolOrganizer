decb2419941a9bd71a2411c454b7ec5e3adc5912
Alexey Sokol <sokol.a.v.30@gmail.com>
2022-12-26 01:19:34 +0200

    [#7] Added Rogue class

--- SokolTextGame/Player.cs
+++ SokolTextGame/Player.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace SokolTextGame
 {
-    internal class Player
+    public interface IPlayer
     {
+        public string Description { get; }
+    }
+    public class Warrior : IPlayer
+    {
+        public string Description { get; } = "A man of unremarkable appearance, but of sturdy build\n" +
+            " and with a facial expression that makes it clear that talking is not your forte.\n" +
+            "You're ready to knock your opponent's teeth out at any moment,\n" +
+            " and maybe even break a bone or two.";
     }
 }
