3552e02008abaf652fe6061832182f5dc42e7e40
Alexey Sokol <sokol.a.v.30@gmail.com>
2022-12-26 02:09:26 +0200

    [#7] Create Weapons interface

---
+++ SokolTextGame/Weapons.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SokolTextGame
+{
+    public interface IWeapon
+    {
+        public string Descriprion { get; }
+    }
+
+    public class BareFists : IWeapon
+    {
+        public string Descriprion { get; } = "Bare fists. What else could it be?";
+    }
+}
