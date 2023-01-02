8d005fa5506e7fe5dbff372b981854cb35415142
Alexey Sokol <sokol.a.v.30@gmail.com>
2023-01-02 22:24:14 +0200

    [#10] Weapons added: axe, sword, staff

--- SokolTextGame/Weapons.cs
+++ SokolTextGame/Weapons.cs
@@ -9,4 +9,16 @@
     {
         public string Description { get; } = "Bare fists. What else could it be?\n";
     }
+    public class Axe : IWeapon
+    {
+        public string Description { get; } = "There is always something to do with an axe. You can split firewood, or you can split a skull.";
+    }
+    public class Sword : IWeapon
+    {
+        public string Description { get; } = "Sword, not big, light, nice to hold in your hand, but you have to watch its sharpness.";
+    }
+    public class Staff : IWeapon
+    {
+        public string Description { get; } = "Staff made of old oak, handle covered with leather, if you know the spell, you can use the staff for additional damage, and if not, you can use it as a two-handed cudgel.";
+    }
 }
